9a44f5cdc2138d3df5693bb6cc8489e093e1dca4
GyroJMCS <gyrojmcs@gmail.com>
2021-12-11 17:16:31 +0100

    Conexión a la BBDD creada

---
+++ Programa.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    conexion = mysql.connector.connect(
+        host="localhost",
+        port=3306,
+        user="root",
+        password="patata123",
+        database="furbo"
+    )
+
+    if conexion.is_connected():
+        print("Conexión correcta :D")
+        inforserver = conexion.get_server_info()
+        print("Info del servidor:", inforserver)
+        cursor = conexion.cursor()
+        cursor.execute("SELECT database();")
+        registro = cursor.fetchone()
+        print("Conectado a la BD:", registro)
+
+except Error as ex:
+    print("Error durante la conexión :(")
+finally:
+    if conexion.is_connected():
+        conexion.close()
+        print("Se cerró la conexión")
